f8d207b944f7df0c13fa5b2af247867c9d41b0bc
maorros <maorros@gmail.com>
2017-08-13 16:11:17 +0200

    red rect 13.8.17

--- small_blocks.py
+++ small_blocks.py
@@ -48,10 +48,17 @@ res = vis.get_resolution()
 #                       sp_step_size=1, sp_num_edge_points_remove=2, sp_resolution=0.05)
 #         fill.draw()
 
-fill = Filler(robot=robot, vis=vis, canvas_ratio=10, board_height=60, board_width=40, \
+# Red rect 13/8/17 16:00
+fill = Filler(robot=robot, vis=vis, canvas_ratio=10, board_height=50, board_width=30, \
               alpha= 0.8, alpha_2 = 0.3, n_switch = 50, \
-                x_translation=170 + 0, y_translation=100 + 50, max_line_length=3000, \
+                x_translation=170 + 100, y_translation=100 + 100, max_line_length=3000, \
                 sp_step_size=50, sp_num_edge_points_remove=2, sp_resolution=0.005)
+
+#
+# fill = Filler(robot=robot, vis=vis, canvas_ratio=10, board_height=60, board_width=40, \
+#               alpha= 0.8, alpha_2 = 0.3, n_switch = 50, \
+#                 x_translation=170 + 0, y_translation=100 + 50, max_line_length=3000, \
+#                 sp_step_size=50, sp_num_edge_points_remove=2, sp_resolution=0.005)
 
 #
 # fill = Filler(robot=robot, vis=vis, canvas_ratio=10, board_height=65, board_width=50, \
