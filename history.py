67e066fd7d07b15c23733cf7fc7cf0c2a29e38b5
maorros <maorros@gmail.com>
2017-08-14 10:09:38 +0200

    history update

--- history.py
+++ history.py
@@ -4,3 +4,9 @@ fill = Filler(robot=robot, vis=vis, canvas_ratio=10, board_height=50, board_widt
                 x_translation=170 + 100, y_translation=100 + 100, max_line_length=3000, \
                 sp_step_size=50, sp_num_edge_points_remove=2, sp_resolution=0.005)
 
+# Red rect 13/8/17 17:00 (same parameters)
+fill = Filler(robot=robot, vis=vis, canvas_ratio=10, board_height=50, board_width=30, \
+              alpha= 0.8, alpha_2 = 0.3, n_switch = 50, \
+                x_translation=170 + 100, y_translation=100 + 100, max_line_length=3000, \
+                sp_step_size=50, sp_num_edge_points_remove=2, sp_resolution=0.005)
+
